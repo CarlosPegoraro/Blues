5b6ee1bcf1ab4e1ddbe79bb00aec3ec42c519e41
Carlos Lopes <carlospegorarolopes@gmail.com>
2023-04-01 02:38:36 -0300

    :guitar: random color block system

--- site/src/components/Information.tsx
+++ site/src/components/Information.tsx
@@ -23,11 +23,33 @@ type Informations = {
 }
 
 export function Informations(props: Informations) {
+    const randomNumber = Math.floor(Math.random() * 4)
+    let randomStyle = ' ';
+    switch (randomNumber) {
+        case 1:
+            randomStyle= "bg-dark text-light";
+            break;
+        case 2:
+            randomStyle = "bg-light text-dark";
+            break;
+        case 3:
+            randomStyle = "bg-primary text-dark";
+            break;
+        case 0:
+            randomStyle = "bg-second text-light";
+            break;
+    
+        default:
+            break;
+    }
+    let finalStyle = "row justify-content-center align-items-center rounded-4 " + randomStyle
     return (
-        <div className=" py-5 my-5 text-center">
-            <h1 className="title pb-5">{props.title}</h1>
-            <div className="col-lg-6 mx-auto">
-            <p className="lead mb-4">{props.text}</p>
+        <div className={finalStyle}>
+            <div className=" py-5 my-5 text-center">
+                <h1 className="title pb-5">{props.title}</h1>
+                <div className="col-lg-6 mx-auto">
+                <p className="lead mb-4">{props.text}</p>
+                </div>
             </div>
         </div>
     );
