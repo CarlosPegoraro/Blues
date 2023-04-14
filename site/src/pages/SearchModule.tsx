b8341e29e874756f7b8755514f7438a0205e4709
CarlosSchool <carlospegorarolopes@gmail.com>
2023-04-14 08:28:35 -0300

    :guitar: update in the search lists

--- site/src/pages/SearchModule.tsx
+++ site/src/pages/SearchModule.tsx
@@ -3,13 +3,12 @@ import { Cards } from '../components/Cards';
 
 /*Images of drinks*/
 
-import zombieImg from '../assets/drinks/zombie.jpg'
-import bloodyMary from '../assets/drinks/bloodyMary.jpg'
 import martinez from '../assets/drinks/martinez.jpg'
 import pinacolada from '../assets/drinks/pinacolada.jpg'
 import sexBeach from '../assets/drinks/sexBeach.jpg'
 import tequilaSunrise from '../assets/drinks/tequilaSunrise.jpg'
 import whiskeySour from '../assets/drinks/whiskeySour.jpg'
+import { BloodyMaryList, ZombieList } from '../components/ListSeach';
 
 interface propsSearchModule {
   keyword: string;
@@ -31,41 +30,21 @@ class SearchModule extends Component<{}, propsSearchModule> {
 
   viewDiv = (): void => {
     const keyword = this.state.keyword.toLowerCase();
-    let div: JSX.Element;
+    let searchDiv: JSX.Element;
     if (keyword === 'zombie') {
-      div = 
+      searchDiv = 
       <section className="container-fluid m-5 mt-0 p-5 mb-0">
-        <h1 className="title my-5">Resultados da Pesquisa</h1>
-        <div className='row'>
-            <Cards src={zombieImg} alt="zombie" title='Zombie' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/zombie' width='18rem' />
-            <Cards src={zombieImg} alt="zombie" title='Zombie' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet." 
-                  href="/drinks/zombie" width='18rem' />
-            <Cards src={zombieImg} alt="zombie" title='Zombie' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/zombie' width='18rem' />
-            <Cards src={zombieImg} alt="zombie" title='Zombie' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/zombie' width='18rem' />
-        </div>
+        <ZombieList/>
       </section>;
     } else if (keyword === 'bloody mary' || keyword === 'bloodymary') {
-      div = 
+      searchDiv = 
       <section className="container-fluid m-5 mt-0 p-5 mb-0">
-        <h1 className="title my-5">Resultados da Pesquisa</h1>
-        <div className='row'>
-            <Cards src={bloodyMary} alt="bloody Mary" title='bloody Mary' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/bloodymary' width='18rem' />
-            <Cards src={bloodyMary} alt="bloody Mary" title='bloody Mary' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/bloodymary' width='18rem' />
-            <Cards src={bloodyMary} alt="bloody Mary" title='bloody Mary' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/bloodymary' width='18rem' />
-            <Cards src={bloodyMary} alt="bloody Mary" title='bloody Mary' text="The quick brown fox jumps over the lazy dog, showcasing all 26 letters of the alphabet."
-                  href='/drinks/bloodymary' width='18rem' />
-        </div>
+        <BloodyMaryList/>
       </section>;
     } else {
-      div = <p className='title text-danger'>drink inexistente</p>;
+      searchDiv = <p className='title text-danger'>drink inexistente</p>;
     }
-    this.setState({ actuallyDiv: div });
+    this.setState({ actuallyDiv: searchDiv });
   };
 
   render() {
